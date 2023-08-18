cbd5f55c57f3de189ddbd30a189d04db0cd77ed9
chabrik abdellatif <abdellatif.chabrik.fycomputing@gmail.com>
2023-08-18 23:36:56 +0100

    sending mails && move sebsitive data to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+//import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        port: 587, // Port for SMTP (587 is common for TLS),
+        secure: false, // Use TLS (true) or not (false)
+        auth: {
+          user: process.env.BOITE_MAIL,
+          pass: process.env.APP_ACCESS_PASSWORD,
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly mailerService: MailerService) {}
+  async getHello(): Promise<any> {
+    try {
+      const p = await this.mailerService.sendMail({
+        to: 'abdoo.chbrik@gmail.com',
+        subject: 'Welcome to our app!',
+        text: 'welcome', // plaintext body
+        html: '<b>welcome</b>', // HTML body content
+      });
+
+      console.log('p', p);
+      return p;
+    } catch (error) {
+      console.log('error', error);
+    }
   }
 }
